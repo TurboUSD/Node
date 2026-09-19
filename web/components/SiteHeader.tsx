f847c5b556c35051cb5cbed3473f3c4bedf9d3ff
TurboUSD <turbousd2024@gmail.com>
2026-09-19 19:47:00 +0200

    Add files via upload

--- web/components/SiteHeader.tsx
+++ web/components/SiteHeader.tsx
@@ -27,6 +27,7 @@ export default function SiteHeader() {
   return (
     <TusdHeader
       site={{ label: '₸USD Network', href: '/' }}
+      subStyle="centered"
       tabs={TABS.map((t) => ({ ...t, active: isActive(t.href) }))}
       Link={Link}
     />
